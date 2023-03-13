0ef123a1b587e44ebfd22e68545e1ad4a0525569
artjoms1 <artjoms.fomins@magebit.com>
2023-03-13 13:32:35 +0200

    error image

--- app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
@@ -1,8 +1,37 @@
 define([
-    "jquery",
-    'ko',
+   // "jquery",
+    //'ko',
    ], function($,ko){
 
     /* code goes here */
     console.log("hello!!!!!!!!!!")
-   });
+    class Qty {
+        constructor() {
+            this.input = document.querySelector(".counter__input")
+            this.value = parseInt(this.input.value,10)
+            console.log(this.value,'valueee')
+            if(isNaN(this.value)) this.value = 0
+        }
+
+        decrement() {
+            if(this.value !== 0) this.value--
+            this.input.value = this.value
+        }
+        increment() {
+
+            console.log(this.input)
+            this.value++
+            this.input.value = this.value
+
+        }
+    }
+    document.querySelector(".counter__changer-minus").addEventListener("click", () => {
+        const counter = new Qty()
+        console.log(counter,counter.input)
+        counter.decrement()
+    })
+    document.querySelector(".counter__changer-plus").addEventListener("click", () => {
+        const counter = new Qty()
+        counter.increment()
+    })
+});
