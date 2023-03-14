39780908cc367def72fc221b151e2a38735f737e
artjoms1 <artjoms.fomins@magebit.com>
2023-03-14 16:06:39 +0200

    commits

--- app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
@@ -1,20 +1,43 @@
-'use strict';
+'use strict'
 
 define([
     'ko',
     'uiElement'
 ], function (ko, Element) {
-    console.log("hello")
+    console.log('hello')
     return Element.extend({
-        defaults: {
-            // template: 'Magento_Catalog/input-counter'
-        },
 
         initObservable: function () {
             this._super()
-                .observe('counterValue');
+                .observe('counterValue')
+                //"buttonDisable": "true",
 
-            return this;
+            this._super().observe('buttonDisable')
+            //let button = ko.observable(false)
+            return this
         },
+        increment: function() {
+            const value = parseInt(this.counterValue(),10)
+            this.counterValue(value + 1)
+            if(value > 0) {
+                document.querySelector('.cart__btn').disabled = false
+            }
+        },
+        decrement: function() {
+            const value = parseInt(this.counterValue(),10)
+            if(value === 1) {
+                document.querySelector('.cart__btn').disabled = true
+            }
+            if(value === 0) {
+                return
+            }
+            this.counterValue(value - 1)
+        },
+        isOverZero: function() {
+            const value = parseInt(this.counterValue(),10)
+            if(value > 0) return false
+
+            return true
+        }
     });
 });
