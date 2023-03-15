e48a3c1067736744c1aa2dba7dcb38db6cf34e41
artjoms1 <artjoms.fomins@magebit.com>
2023-03-15 18:25:59 +0200

    changes after review

--- app/code/Magebit/PageListWidget/Block/Widget/PageList.php
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Magebit_PageListWidget
+ *
+ * @category     Magebit
+ * @package      Magebit_PageListWidget
+ * @author       Artjoms Fomins <info@magebit.com>
+ * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
+ */
+
+declare(strict_types=1);
+
 namespace Magebit\PageListWidget\Block\Widget;
 
 use Magento\Framework\View\Element\Template;
@@ -8,12 +19,31 @@ use Magento\Framework\Data\OptionSourceInterface;
 use Magento\Cms\Api\PageRepositoryInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 
-class PageList extends Template implements OptionSourceInterface,BlockInterface
+/**
+ * Class that responsible for module logic
+ *
+ */
+class PageList extends Template implements OptionSourceInterface, BlockInterface
 {
-    protected $_template = "page-list.phtml";
+    /**
+     * @var string */
+    protected $template = "page-list.phtml";
+    /**
+     * @var PageRepositoryInterface */
     private PageRepositoryInterface $pageRepositoryInterface;
+
+    /**
+     * @var SearchCriteriaBuilder */
     private SearchCriteriaBuilder $searchCriteriaBuilder;
 
+    /**
+     * Cms constructor
+     *
+     * @param PageRepositoryInterface $pageRepositoryInterface
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param Template\Context $context
+     * @param array $data
+     */
     public function __construct(
         PageRepositoryInterface $pageRepositoryInterface,
         SearchCriteriaBuilder $searchCriteriaBuilder,
@@ -24,40 +54,49 @@ class PageList extends Template implements OptionSourceInterface,BlockInterface
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         parent::__construct($context, $data);
     }
-
-    public function toOptionArray()
+    /**
+     * Function which return cms pages in prettier version
+     *
+     * @return array
+     */
+    public function toOptionArray(): array
     {
 
         $optionArray = [];
-            $pages = $this->getCmsPageCollection();
-            $cnt = 0;
-            foreach ($pages as $page) {
-                $optionArray[$cnt]['value'] = $page->getIdentifier();
-                $optionArray[$cnt++]['label'] = $page->getTitle();
-            }
+        $pages = $this->getCmsPageCollection();
+        $cnt = 0;
+        foreach ($pages as $page) {
+            $optionArray[$cnt]['value'] = $page->getIdentifier();
+            $optionArray[$cnt++]['label'] = $page->getTitle();
+        }
         return $optionArray;
     }
-    protected function getCmsPageCollection()
+    /**
+     * Function that return all cms pages
+     *
+     * @return array
+     */
+    protected function getCmsPageCollection(): array
     {
         $searchCriteria = $searchCriteria = $this->searchCriteriaBuilder->create();
         return $this->pageRepositoryInterface->getList($searchCriteria)->getItems();
     }
 
     /**
-     * Return the filtered array of selected pages
+     * Return the filtered array of selected pages depening of selected user's pages
      *
      * @return array
      */
     public function getListOfPages()
     {
-        if($this->getData('display') === 'all') {
+        if ($this->getData('display') === 'all') {
             return $this->toOptionArray();
         }
 
-        $selectedPages = explode(',', $this->getData('display_pages') );
+        $selectedPages = explode(',', $this->getData('display_pages'));
 
-        return array_filter($this->toOptionArray(), function($v) use ($selectedPages) {
-            if(in_array($v['value'],$selectedPages)) {
+        return array_filter($this->toOptionArray(), function ($v) use ($selectedPages) {
+            if (in_array($v['value'], $selectedPages)) {
                 return $v;
             }
         });
--- app/code/Magebit/PageListWidget/registration.php
+++ app/code/Magebit/PageListWidget/registration.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Magebit_PageListWidget
+ *
+ * @category     Magebit
+ * @package      Magebit_PageListWidget
+ * @author       Artjoms Fomins <info@magebit.com>
+ * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
+ */
+
+declare(strict_types=1);
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
