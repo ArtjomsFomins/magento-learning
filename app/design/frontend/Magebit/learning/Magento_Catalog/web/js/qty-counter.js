8fea5aa910b799b33ad307801825bd07f18cd375
artjoms1 <artjoms.fomins@magebit.com>
2023-03-18 13:31:49 +0200

    playing with vscode default formatting

--- app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
@@ -1,81 +1,81 @@
-"use strict";
+'use strict'
 
-define(["ko", "uiElement"], function (ko, Element) {
-    const input = document.querySelector(".counter-input");
-    const button = document.querySelector(".cart-btn");
-    input.addEventListener("keydown", (event) => {
+define(['ko', 'uiElement'], function (ko, Element) {
+    const input = document.querySelector('.counter-input')
+    const button = document.querySelector('.cart-btn')
+    input.addEventListener('keydown', event => {
         const codes = [
             190, // .
             69, // e (scientific notaion, 1e2 === 100)
-        ];
+        ]
 
         if (codes.includes(event.keyCode)) {
-            event.preventDefault();
+            event.preventDefault()
 
-            return false;
+            return false
         }
 
-        return true;
-    });
+        return true
+    })
 
-    input.addEventListener("input", function (e) {
-        var reg = /^(?!0)[0-9]*$/;
-        const result = this.value.match(reg);
+    input.addEventListener('input', function (e) {
+        var reg = /^(?!0)[0-9]*$/
+        const result = this.value.match(reg)
 
-        if (!result || result[0] === "") {
-            this.value = this.value.slice(0, -1);
-            this.value = this.value.replace(reg, "");
+        if (!result || result[0] === '') {
+            this.value = this.value.slice(0, -1)
+            this.value = this.value.replace(reg, '')
         }
 
-        const intValue = parseInt(result, 10);
+        const intValue = parseInt(result, 10)
 
         if (intValue > 0) {
-            button.disabled = false;
+            button.disabled = false
         } else if (intValue === 0 || isNaN(intValue)) {
-            button.disabled = true;
-            this.value = "0";
+            button.disabled = true
+            this.value = '0'
         }
-    });
+    })
 
     return Element.extend({
         initObservable: function () {
-            this._super().observe("counterValue");
+            this._super().observe('counterValue')
 
-            return this;
+            return this
         },
         increment: function () {
-            let value = this.getValue();
-            value++;
-            this.counterValue(value);
+            let value = this.getValue()
+            value++
+            this.counterValue(value)
 
             if (value > 0) {
-                button.disabled = false;
+                button.disabled = false
             }
         },
         getValue: function () {
-            return parseInt(this.counterValue(), 10);
+            return parseInt(this.counterValue(), 10)
         },
         decrement: function () {
-            const value = this.getValue();
+            const value = this.getValue()
 
             if (value === 1) {
-                button.disabled = true;
+                button.disabled = true
             }
 
             if (value === 0) {
-                return;
+                return
             }
 
-            this.counterValue(value - 1);
+            this.counterValue(value - 1)
         },
         isOverZero: function () {
-            const value = this.getValue();
+            const value = this.getValue()
 
             if (value > 0) {
-                return false;
+                return false
             }
 
-            return true;
+            return true
         },
-    });
-});
+    })
+})
