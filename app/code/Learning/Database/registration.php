15bc02eb72b12a2242b8161851328d55b541b786
artjoms1 <artjoms.fomins@magebit.com>
2023-03-20 15:43:58 +0200

    start of ml02

---
+++ app/code/Learning/Database/registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Learning_Database
+ *
+ * @category     Magebit
+ * @package      Learning_Database
+ * @author       Artjoms Fomins <info@magebit.com>
+ * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Learning_Database', __DIR__);
