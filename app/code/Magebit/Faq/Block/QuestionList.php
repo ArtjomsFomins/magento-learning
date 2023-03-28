d4efffe17ec8fa85d090d1a24e7ac04fa88cad83
artjoms1 <artjoms.fomins@magebit.com>
2023-03-28 15:02:00 +0300

    save and close button

--- app/code/Magebit/Faq/Block/QuestionList.php
+++ app/code/Magebit/Faq/Block/QuestionList.php
@@ -7,13 +7,14 @@
 namespace Magebit\Faq\Block;
 
 use Magento\Framework\View\Element\AbstractBlock;
+use Magento\Widget\Block\BlockInterface;
 
 /**
  * Cms block content block
  * @deprecated This class introduces caching issues and should no longer be used
  * @see \Magebit\Faq\Block\BlockByIdentifier
  */
-class QuestionList extends \Magento\Framework\View\Element\Template
+class QuestionList extends \Magento\Framework\View\Element\Template implements BlockInterface
 {
     public function _prepareLayout()
     {
--- app/code/Magebit/Faq/Controller/Index/Index.php
+++ app/code/Magebit/Faq/Controller/Index/Index.php
@@ -41,6 +41,40 @@ class Index extends \Magento\Backend\App\Action
      */
     public function execute(): Page
     {
-        return $this->pageFactory->create(ResultFactory::TYPE_PAGE);
+        return $this->pageFactory->create();
     }
 }
+
+// use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+// use Magento\Backend\App\Action\Context;
+// use Magento\Framework\View\Result\PageFactory;
+
+// class Index extends \Magento\Backend\App\Action
+// {
+//     /**
+//      * @var PageFactory
+//      */
+//     protected $resultPageFactory;
+
+//     /**
+//      * @param Context $context
+//      * @param PageFactory $resultPageFactory
+//      */
+//     public function __construct(
+//         Context $context,
+//         PageFactory $resultPageFactory
+//     ) {
+//         $this->resultPageFactory = $resultPageFactory;
+//         parent::__construct($context);
+//     }
+
+//     /**
+//      * Default customer account page
+//      *
+//      * @return \Magento\Framework\View\Result\Page
+//      */
+//     public function execute()
+//     {
+//         return $this->resultPageFactory->create();
+//     }
+// }
